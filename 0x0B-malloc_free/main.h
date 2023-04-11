9493810fcb036eb6db79fa50f7ddc1faeeb264c0
Kheireddine-Anas <kheireddine.anas@gmail.com>
2023-04-11 22:04:45 +0000

    dimensional array of integers task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,5 +5,6 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char **strtow(char *str);
 char *argstostr(int ac, char **av);
+int **alloc_grid(int width, int height);
 
 #endif
