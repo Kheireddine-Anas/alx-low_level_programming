3f114e6555a404145ea110cd2ea6e04cc297818d
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-05-04 18:54:26 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,10 +10,10 @@ unsigned int binary_to_uint(const char *b) {
 	unsigned int num = 0;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 	while (*b) {
 		if (*b != '0' && *b != '1')
-			return 0;
+			return (0);
 
 		num <<= 1;
 		if (*b == '1')
