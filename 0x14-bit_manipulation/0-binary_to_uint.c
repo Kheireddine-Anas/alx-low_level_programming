fa585c41e3c49dacb1c86fa23fcc196772eb2324
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-05-05 19:53:02 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,12 +6,14 @@
  * @b: char string
  * Return: converted decimal number or 0 if there is an unconvertable char
  */
-unsigned int binary_to_uint(const char *b) {
+unsigned int binary_to_uint(const char *b)
+{
 	unsigned int num = 0;
 
 	if (b == NULL)
 		return (0);
-	while (*b) {
+	while (*b)
+	{
 		if (*b != '0' && *b != '1')
 			return (0);
 
