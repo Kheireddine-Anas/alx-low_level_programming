f2a8d7f0576718ef971c5acfc7ef29f5b6fc0f7b
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-05-08 13:39:27 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: KHEIREDDINE Anas
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x15-file_io directory.
- */
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
