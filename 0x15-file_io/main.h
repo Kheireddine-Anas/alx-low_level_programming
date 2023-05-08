5ce81abb563c9f61b750bc8c72abc7380b6f24b9
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-05-08 15:02:03 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: KHEIREDDINE Anas
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x15-file_io directory.
- */
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
